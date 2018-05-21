dcf2ff03b0c829fd601139f056a1b18fbb09a6e2
Gary Hendrick <gary.hendrick@gmail.com>
2018-05-21 11:40:06 -0400

    scrpting to permit easy pan and tilt control

---
+++ scripts/tilt_scan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: 0.25 z: 0.0"  -1
+rostopic pub /ptu/rotate_relative geometry_msgs/Twist "linear: x: 0.0 y: 0.0 z: 0.0 angular: x: 0.0 y: -0.25 z: 0.0"  -1
+
